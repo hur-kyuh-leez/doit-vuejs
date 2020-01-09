88191167a45c2057016949d86290232653a6db1a
Hercules <hyukkyulee@alumni.emory.edu>
2020-01-09 15:51:14 +0900

    camelcase in js

--- final/vue-todo-myversion/src/components/TodoInput.vue
+++ final/vue-todo-myversion/src/components/TodoInput.vue
@@ -25,6 +25,11 @@
 /* 여기 뭐지 Modal import 하고 component에 등록을 하는데 
 정작 <template>에서 <Modal>을 찾을 수 없고 <modal>만 보인다. 
 그런데도 잘 작동한다. Modal이 하나 다른점은 export default가 없다는 점?
+https://vuejs.org/v2/guide/components-props.html#Prop-Casing-camelCase-vs-kebab-case
+
+camelCase in js
+should be kebab-case in html
+
 */
 import Modal from "./common/Modal.vue";
 export default {
@@ -38,6 +43,8 @@ export default {
     addTodo() {
       if (this.newTodoItem !== "") {
         var value = this.newTodoItem && this.newTodoItem.trim(); //.trim remove unecssary blanks. ex)  "     Hello World!     " --> "Hello World!" 그런데 왜 아직 스페이스 하나는 등록이 될까?
+        /* 여기서 addTodo는  v-on:addTodo 이다
+         not v-on:addTodo 뒤에있는 "addTodo" */
         this.$emit("addTodo", value);
         //여기서 더 이상  localStorage를 안하고 상위에서 처리한다. localStorage.setItem(value, value); // .setItem(keyName, keyValue);
       } else {
